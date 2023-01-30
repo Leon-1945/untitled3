3800abf33fb92400834b58d0c3d5f1c2f60cb20e
Leon1945 <Muravlevevgeniy@yandex.ru>
2023-01-30 20:29:03 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+         int price = 200;
+
+         int miles = price / 20;
+         System.out.println("бонусы: " + miles);
+    }
+}
